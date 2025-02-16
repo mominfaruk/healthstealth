34037170f47d574230756242a6a528683cce87fe
Al Momin Faruk <31950303+mominfaruk@users.noreply.github.com>
2025-02-16 23:45:29 +0600

    Add versioned API structure for authentication and update URL routing

--- healthstealth/api_urls.py
+++ healthstealth/api_urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.contrib import admin
+from apps.authentication.versioned_api.v1.routers import urlpatterns as authentication_urlpatterns
 
 version = 'v1'
 api_urlpatterns = [
     path(f'api/{version}/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path(f'api/{version}/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path(f'api/{version}/auth/', include(authentication_urlpatterns)),
+]
+
+admin_urlpatterns = [
+    path('admin/', admin.site.urls),
 ]
--- healthstealth/urls.py
+++ healthstealth/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin  # noqa: admin is not accessed
+from django.urls import path, include  # noqa: path, include are not accessed
+from django.conf import settings
+from django.conf.urls.static import static
+from .api_urls import api_urlpatterns, admin_urlpatterns
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = api_urlpatterns + admin_urlpatterns
+
+urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns  += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
